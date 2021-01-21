bcce9fe7131fd7b7d7490886fd65394b49791887
Faruk <farukmd494@gmail.com>
2021-01-21 15:51:28 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Face Detection using Haar Cascade Classifiers
+import cv2
+face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#img=cv2.imread('lina.png')
+cap=cv2.VideoCapture(0)
+while cap.isOpened():
+    _,img=cap.read()
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray,1.1,4)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
+
+    cv2.imshow('img',img)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
